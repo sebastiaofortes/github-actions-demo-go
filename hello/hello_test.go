473b9e9c4d3c7b6a3b953c3eef5b14e85625cc7e
Sebastião Fortes <sebastiaofortes4@gmail.com>
2022-11-16 10:03:49 -0300

    novo teste 3

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -22,3 +22,10 @@ func TestGreetsGitHub3(t *testing.T) {
 		t.Errorf("Greet() = %s; want Hello GitHub Actions. Dev.to is awesome", result)
 	}
 }
+
+func TestGreetsGitHub4(t *testing.T) {
+	result := Greet()
+	if result != "Hello GitHub Actions. Dev.to is awesome" {
+		t.Errorf("Greet() = %s; want Hello GitHub Actions. Dev.to is awesome", result)
+	}
+}
