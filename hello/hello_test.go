566f0178f17246f865ac03b78a24269c2827271e
Sebastião Fortes <sebastiaofortes4@gmail.com>
2022-11-16 22:52:28 -0300

    gitflow

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -25,14 +25,12 @@ func TestGreetsGitHub3(t *testing.T) {
 		t.Errorf("Greet() = %s; want Hello GitHub Actions. Dev.to is awesome", result)
 	}
 }
-
 func TestGreetsGitHub4(t *testing.T) {
 	result := Greet()
 	if result != "Hello GitHub Actions. Dev.to is awesome" {
 		t.Errorf("Greet() = %s; want Hello GitHub Actions. Dev.to is awesome", result)
 	}
 }
-
 func TestSoma(t *testing.T) {
 	inteiros := []int64{1,2,3,4,5}
 	fmt.Println(Soma(inteiros))
